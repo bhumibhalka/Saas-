ea71945cc406b4dc84734493d9c4851eb162f9bf
Bhumi <bhumibhalka@gmail.com>
2026-05-12 02:53:45 +0530

    setup

---
+++ meetai/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="text-red-300">
+      Hello
+
+      <Button variant={"default"} >Click me</Button>
+    </div>
+
+  );
+}
